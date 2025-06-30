c4c6fd71569f05780946a1d4929f2990217f009e
Pratit Bangdiwala <pratit.bangdiwala@neosoftmail.com>
2025-06-30 11:44:36 +0530

    fix: formatting

--- src/components/loginInput/loginInput.tsx
+++ src/components/loginInput/loginInput.tsx
@@ -26,38 +26,18 @@ const LoginInput = ({
   const updateVisibilityState = () => {
     setVisible((prev) => !prev);
   };
-  const currentType =
-    inputName === "userPassword" && visible ? "text" : inputType;
+  const currentType = inputName === "userPassword" && visible ? "text" : inputType;
+  console.log(inputValue, 'inputValue')
 
   return (
-    <div
-      className={`${inputCss["lip-input__wrap"]} ${
-        inputValue ? inputCss["lip-input--filled"] : ""
-      }`}
-    >
-      <input
-        type={currentType}
-        name={inputName}
-        id={inputId}
-        placeholder={inputPlaceholder}
-        className={`${inputCss["lip-input__inputBox"]}`}
-        onChange={getInputValue}
-        disabled={freezeInput}
-      />
-      <label htmlFor={inputId} className={`${inputCss["lip-input__label"]}`}>
-        {inputLabel}
-      </label>
-      {icon === "passwordEye" && (
-        <span
-          className={`${inputCss[`${customCls}`]} ${
-            inputCss["lip-input__icon"]
-          }`}
-          onClick={updateVisibilityState}
-        >
-          {visible ? <IoEyeOffOutline size={20} /> : <IoEyeOutline size={20} />}
-        </span>
-      )}
-    </div>
+    <div className={`${inputCss["lip-input__wrap"]} ${inputValue ? inputCss["lip-input--filled"] : ""}`}>
+      <input type={currentType} name={inputName} id={inputId} placeholder={inputPlaceholder} className={`${inputCss["lip-input__inputBox"]}`} onChange={getInputValue} disabled={freezeInput} />
+      <label htmlFor={inputId} className={`${inputCss["lip-input__label"]}`}>{inputLabel}</label>
+      {icon === "passwordEye" ? (<span className={`${inputCss[`${customCls}`]} ${inputCss["lip-input__icon"]}`} onClick={updateVisibilityState}>{visible ? <IoEyeOffOutline size={20} /> : <IoEyeOutline size={20} />}</span>
+      )
+        : ''
+      }
+    </div >
   );
 };
 

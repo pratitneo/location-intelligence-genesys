fb84a7162d8e98207a6a8514344bf79f488b578b
Mahendra-7-Neosoft <mahendra.jadav@neosoftmail.com>
2025-07-29 12:44:31 +0530

    fix: format chat response and fix layout page error.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -117,7 +117,7 @@ const Layout = () => {
           {/* Left Sidebar */}
           <Sidebar getToggleFn={toggleSidebar} onIconClick={handleIconClick} sidebarOpen={panelName === 'menubar'} sideText={sideTextVisibility} />
           {/* Left SidepanelName */}
-          <SidePanel customCls="left" visibleCls="left" visible={panelName === 'leftPanel'} content={GeoAnalysisContent} contentProps={{ setAreaBoundary: setPincodeBoundary, setPosition, setZoom }} />
+          <SidePanel customCls="left" visibleCls="left" visible={panelName === 'leftPanel'} content={<GeoAnalysisContent setAreaBoundary={setPincodeBoundary} setPosition={setPosition} setZoom={setZoom} />} />
           {/* Right Sidebar */}
           <RightSideBar onIconClick={handleIconClick} handleIconClick={toggleLeftPanel} isPanelOpen={panelName === 'rightPanel'} activeKey={subIconKey ? subIconKey : rightIconKey} />
           {/* Right Side Panel */}

a00410565d19a7eaa8f41e51d895a3a89ee27756
Pratit Bangdiwala <pratit.bangdiwala@neosoftmail.com>
2025-07-29 16:10:49 +0530

    fix: git base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/location-intelligence-genesys/', // 👈 important for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {

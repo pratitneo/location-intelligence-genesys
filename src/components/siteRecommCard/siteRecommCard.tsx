c240189780bcd3bebda73a2e93d03a6320240b1b
Pratit Bangdiwala <pratit.bangdiwala@neosoftmail.com>
2025-07-31 15:25:07 +0530

    feat: ai recos,

--- src/components/siteRecommCard/siteRecommCard.tsx
+++ src/components/siteRecommCard/siteRecommCard.tsx
@@ -1,19 +1,29 @@
+import { Images } from '../../assets/assets'
+import type { RecommCardType } from '../../types/types'
+import ButtonIcon from '../buttonIcon/buttonIcon'
 import recomCardCss from './siteRecommCard.module.scss'
 
-const SiteRecommCard = () => {
+const SiteRecommCard = ({ cardData }: RecommCardType) => {
     return (
         <div className={`${recomCardCss['lip-recomCard__wrap']}`}>
+            {/* location & score */}
             <div className={`${recomCardCss['lip-recomCard__header']}`}>
-                <p className={`${recomCardCss['lip-recomCard__location']}`}></p>
-                <span className={`${recomCardCss['lip-recomCard__score']}`}></span>
+                <p className={`${recomCardCss['lip-recomCard__location']}`}>{cardData?.location}</p>
+                <p className={`${recomCardCss['lip-recomCard__outOfScore']}`}><span className={`${recomCardCss['lip-recomCard__score']}`}>{cardData?.score}</span>/10</p>
             </div>
-            <div className={`${recomCardCss['lip-recomCard__pinCity']}`}>
-                <div className={`${recomCardCss['lip-recomCard__pincode']}`}>
-                    pincode: <span className={`${recomCardCss['lip-recomCard__pinNum']}`}></span>
+            {/* pincode & city */}
+            <div className={`${recomCardCss['lip-recomCard__pinCityWrap']}`}>
+                <div className={`${recomCardCss['lip-recomCard__pinCity']}`}>
+                    pincode: <span className={`${recomCardCss['lip-recomCard__pinNum']}`}>{cardData?.pincode}</span>
                 </div>
-                <div className={`${recomCardCss['lip-recomCard__city']}`}>
-                    city: <span className={`${recomCardCss['lip-recomCard__cityName']}`}></span>
+                <div className={`${recomCardCss['lip-recomCard__pinCity']}`}>
+                    city: <span className={`${recomCardCss['lip-recomCard__cityName']}`}>{cardData?.city}</span>
                 </div>
+            </div>
+            <div className={`${recomCardCss['lip-recomCard__btns']}`}>
+                <ButtonIcon bgCls='violet' btnText='locate' mainCls='recommCard' trailIcon={Images?.locate} />
+                <ButtonIcon bgCls='violet' btnText='shortlist' mainCls='recommCard' trailIcon={Images?.shortlist} />
+                <ButtonIcon bgCls='red' btnText='reject' mainCls='recommCard' trailIcon={Images?.reject} />
             </div>
         </div>
     )

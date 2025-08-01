a508359bce372901b108103906525295cddf1062
Pratit Bangdiwala <pratit.bangdiwala@neosoftmail.com>
2025-08-01 12:15:48 +0530

    fix: unused code, button icon component, drpdwn component, type errors

--- src/components/sidePanel/sidePanel.tsx
+++ src/components/sidePanel/sidePanel.tsx
@@ -1,14 +1,6 @@
 import type { SidePanelType } from "../../types/types";
 import styles from "./sidePanel.module.scss";
 
-// export type SidePanelType = {
-//   customCls: string;
-//   visible: boolean;
-//   visibleCls: string;
-//   content: React.ReactNode;
-//   contentProps?: any;
-// };
-
 const SidePanel = ({ customCls, visible, visibleCls, content }: SidePanelType) => {
   return (
     <div className={` ${styles['lip-panel__wrap']} ${styles[`lip-panel__${customCls}`]} ${visible ? styles[`lip-panel--${visibleCls}`] : ""}`}>
